aea99c80d5e0a352ebe38afafda800e1a6d348d4
Sean <seanpapanikolas@gmail.com>
2016-12-21 07:45:10 -0800

    update build

--- node_scripts/run-build.js
+++ node_scripts/run-build.js
@@ -1,11 +1,16 @@
 const webpack = require('webpack');
 const config = require('../webpack.config.js');
-const compiler = webpack(config.map(obj => Object.assign({}, obj, {
+const configs = config.map(obj => Object.assign({}, obj, {
     debug: false,
     devtool: null,
     plugins: [],
     output: Object.assign({}, obj.output, { path: './' })
-})));
+}));
+
+console.log(configs);
+
+
+const compiler = webpack(configs);
 
 compiler.run((err, stats) => {
     console.log(stats.toString({
