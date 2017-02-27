708155243d8f9320f44f9789702315d65c859040
Sean <seanpapanikolas@gmail.com>
2017-02-26 17:25:43 -0800

    sending object when error occurs

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,10 @@ import Commander from './commander';
 const createService = (name, services) => ({ name, services });
 
 const promiseResolve = res => data => res.json(data)
-const promiseReject = res => e => res.status(e.code || 500).send(e.message || '');
+const promiseReject = res => e => res.status(e.code || 500).send({
+    message: e.message || 'error',
+    statusCode: e.code || 500,
+});
 
 const handleRoute = commander => (req, res, next) => {
     commander.exec(req.params.service, req.body.action, req.body.payload, req, res, next).then(promiseResolve(res), promiseReject(res)).catch(promiseReject(res));
